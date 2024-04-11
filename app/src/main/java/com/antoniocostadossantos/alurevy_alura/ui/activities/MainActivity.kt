d94446d080763acd721245f5f229eb7ea9a2bf79
Antonio <antonio195br@gmail.com>
2024-04-10 21:26:28 -0300

    Tela de novo produto finalizada

--- app/src/main/java/com/antoniocostadossantos/alurevy_alura/ui/activities/MainActivity.kt
+++ app/src/main/java/com/antoniocostadossantos/alurevy_alura/ui/activities/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.antoniocostadossantos.alurevy_alura
+package com.antoniocostadossantos.alurevy_alura.ui.activities
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
---
+++ app/src/main/java/com/antoniocostadossantos/alurevy_alura/ui/activities/ProductFormActivity.kt
@@ -0,0 +1,225 @@
+package com.antoniocostadossantos.alurevy_alura.ui.activities
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.antoniocostadossantos.alurevy_alura.R
+import com.antoniocostadossantos.alurevy_alura.model.Product
+import com.antoniocostadossantos.alurevy_alura.ui.activities.ui.theme.AlurevyaluraTheme
+import java.math.BigDecimal
+import java.text.DecimalFormat
+import java.util.Formatter
+
+class ProductFormActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AlurevyaluraTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ProductFormScreen()
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProductFormScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 8.dp)
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+
+        var url by remember { mutableStateOf("") }
+        var name by remember { mutableStateOf("") }
+        var price by remember { mutableStateOf("") }
+        var description by remember { mutableStateOf("") }
+
+        val formatter = remember { DecimalFormat("#.##") }
+
+        Text(
+            text = "Novo produto",
+            textAlign = TextAlign.Center,
+            fontSize = 24.sp,
+            fontWeight = FontWeight(500),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp)
+        )
+
+        if (url.isNotBlank()) {
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .border(width = 1.dp, color = Color.Gray, shape = RoundedCornerShape(2))
+                    .padding(vertical = 16.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                Text(
+                    text = "Preview",
+                    textAlign = TextAlign.Center,
+                    fontSize = 18.sp,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                AsyncImage(
+                    model = url,
+                    contentDescription = null,
+                    contentScale = ContentScale.Fit,
+                    placeholder = painterResource(id = R.drawable.placeholder),
+                    error = painterResource(id = R.drawable.placeholder),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .heightIn(max = 200.dp)
+                )
+            }
+        }
+
+        OutlinedTextField(
+            value = url, onValueChange = { url = it },
+            placeholder = {
+                Text(text = "URL da imagem")
+            },
+            label = {
+                Text(text = "URL")
+            },
+            trailingIcon = {
+                Icon(
+                    imageVector = Icons.Outlined.Delete,
+                    contentDescription = null,
+                    modifier = Modifier.clickable { url = "" },
+                )
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Uri,
+                imeAction = ImeAction.Next
+            ),
+            singleLine = true,
+            maxLines = 1,
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+
+        OutlinedTextField(
+            value = name, onValueChange = { name = it },
+            placeholder = {
+                Text(text = "Nome do produto")
+            },
+            label = {
+                Text(text = "Nome")
+            },
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Next,
+                capitalization = KeyboardCapitalization.Words
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+
+        OutlinedTextField(
+            value = price, onValueChange = {
+                try {
+                    price = formatter.format(BigDecimal(it))
+                } catch (e: IllegalArgumentException) {
+                    if (it.isBlank()) price = it
+                }
+            },
+            placeholder = {
+                Text(text = "Preço do produto")
+            },
+            label = {
+                Text(text = "Preço")
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Decimal,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+
+        OutlinedTextField(
+            value = description, onValueChange = { description = it },
+            placeholder = {
+                Text(text = "Descrição do produto")
+            },
+            label = {
+                Text(text = "Descrição")
+            },
+            keyboardOptions = KeyboardOptions(
+                capitalization = KeyboardCapitalization.Sentences
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .heightIn(min = 150.dp)
+        )
+
+        OutlinedButton(
+            onClick = {
+                val product = Product(
+                    name = name,
+                    price = BigDecimal(price),
+                    image = url,
+                    description = description
+                )
+                Log.i("PRODUTO", "Produto = $product")
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .heightIn(60.dp)
+                .padding(vertical = 8.dp)
+        ) {
+            Text(
+                text = "Cadastrar produto",
+                fontSize = 18.sp
+            )
+        }
+    }
+}
+
